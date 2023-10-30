eb56d866044469026d418d455f7797165fb19570
SunilVerma0358 <sunilverma93060@gmail.com>
2023-10-30 15:40:31 +0530

    complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,34 +15,34 @@ var myFour = document.getElementById("myFour");
 open.addEventListener("click", function () {
     modal.style.display = "block",
         overlay.style.display = "block"
-    flow.style.overflow = "hidden"
+
 });
 close.addEventListener("click", function () {
     modal.style.display = "none";
     overlay.style.display = "none"
-    flow.style.overflow = "unset"
+
 
 });
 second.addEventListener("click", function () {
     mySecond.style.display = "block",
         overlay.style.display = "block"
-    flow.style.overflow = "hidden"
+
 });
 seconded.addEventListener("click", function () {
     mySecond.style.display = "none";
     overlay.style.display = "none"
-    flow.style.overflow = "unset"
+
 
 });
 third.addEventListener("click", function () {
     myThird.style.display = "block",
         overlay.style.display = "block"
-    flow.style.overflow = "hidden"
+
 });
 thired.addEventListener("click", function () {
     myThird.style.display = "none";
     overlay.style.display = "none"
-    flow.style.overflow = "unset"
+
 
 });
 four.addEventListener("click", function () {
